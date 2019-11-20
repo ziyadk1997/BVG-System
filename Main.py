91eea066540f686671862239d0490caa93012c63
ziyadk1997 <ziyadk@windowslive.com>
2019-11-20 21:19:01 +0200

    Added Greedy Search

--- Main.py
+++ Main.py
@@ -37,6 +37,87 @@ class Graph:
 	def __init__(self): 
 		# default dictionary to store graph 
 		self.graph = defaultdict(Node) 
+	#Greedy Traversal
+	def Greedy(self,startNode,goalNode):
+		expandedNodes = 0
+		startTimeGreedy = time.time()
+		#create list to put Nodes in
+		list = []
+		#add first Node and mark as visited
+		self.graph[startNode].visited = True
+		list.append(self.graph[startNode])
+		lat1 = latList[startNode]
+		lon1 = longList[startNode]
+		lat2 = latList[goalNode]
+		lon2 = longList[goalNode]
+		dlon = lon2 - lon1
+		dlat = lat2 - lat1
+		a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+		c = 2 * atan2(sqrt(a), sqrt(1 - a))
+		self.graph[startNode].heuristic = R * c
+		#Initialize Goal Node
+		goal = Node(-1)
+		#while list not empty
+		while list:
+			expandedNodes+=1
+			#remove element with lowest heuristic cost
+			currentNode = list[0]
+			for node in list:
+				if node.heuristic < currentNode.heuristic:
+					currentNode = node
+			list.remove(currentNode)
+			#check if goal
+			if goalNode == currentNode.id:
+				goal = self.graph[currentNode.id]
+				break
+			# Get possible Moves for current Node
+			possibleMovesArray = self.graph[currentNode.id].possibleMoves
+			#push every possible move if not visited before
+			for possibleMove in possibleMovesArray:
+				if self.graph[possibleMove].visited == False:
+					lat1 = latList[possibleMove]
+					lon1 = longList[possibleMove]
+					lat2 = latList[goalNode]
+					lon2 = longList[goalNode]
+					dlon = lon2 - lon1
+					dlat = lat2 - lat1
+					a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+					c = 2 * atan2(sqrt(a), sqrt(1 - a))
+					self.graph[possibleMove].heuristic = R * c
+					self.graph[possibleMove].parentNode = self.graph[currentNode.id]
+					list.append(self.graph[possibleMove]) 
+					self.graph[possibleMove].visited = True
+		#If Goal Found
+		if not(goal.id ==-1):
+			path = []
+			#Tranverse Parents of Goal Node till start Node is reached and print path
+			while not (goal is None):
+				#print(goal.id)
+				path.insert(0,goal)
+				goal = goal.parentNode
+			currentUbahnLine = path[0].Us[0]
+			drive1 = path[0].id
+			drive2 = path[0].id
+			costOfTrip = 0
+			for i in range(0,len(path)-1):
+				if(path[i+1].Us.__contains__(currentUbahnLine)):
+					costOfTrip+=path[i].possibleMovesCost[path[i].possibleMoves.index(path[i+1].id)]
+					drive2 = path[i+1].id
+					if i == len(path)-2:
+						if currentUbahnLine == 10:
+							currentUbahnLine= 55
+						print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
+				else:
+					costOfTrip +=5
+					print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
+					drive1 = drive2
+					drive2 = path[i+1].id
+					commonUbahnLine = set(path[i].Us).intersection(path[i+1].Us).pop()
+					print("Switch from U"+str(currentUbahnLine)+" To U"+str(commonUbahnLine))
+					currentUbahnLine = commonUbahnLine
+			print("Cost of Trip is :" + str(costOfTrip))	
+		print("Greedy Searching Time is " + str(time.time()-startTimeGreedy))
+		print("Expanded Nodes : " + str(expandedNodes))
   #BFS Traversal 
 	def BFS(self,startNode,goalNode): 
 		startTimeBFS = time.time()
