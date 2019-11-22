898b6946311931a4b313aa55b31e0f591debf551
ziyadk1997 <ziyadk@windowslive.com>
2019-11-22 20:59:23 +0200

    Added Queue state

--- Main.py
+++ Main.py
@@ -37,65 +37,6 @@ class Graph:
 	def __init__(self): 
 		# default dictionary to store graph 
 		self.graph = defaultdict(Node) 
-	#DFS Traversal 
-	def DFS(self,startNode,goalNode): 
-		expandedNodes = 0
-		startTimeDFS = time.time()
-		# Create a stack for DFS 
-		stack = [] 
-		#push first startNode to start traversing DFS
-		self.graph[startNode].visited = True
-		stack.append(startNode)
-		# Initialize goal Node
-		goal = Node(-1)
-        # traversing the stack until its empty
-		while stack: 
-			expandedNodes+=1
-			# pop an element
-			currentNode = stack.pop()
-			# check if it is a goal if it is break from the loop goal found
-			if int(goalNode) == int(currentNode):
-				goal = self.graph[currentNode]
-				break
-			# Get possible Moves for current Node
-			possibleMovesArray = self.graph[currentNode].possibleMoves
-			#push every possible move if not visited before
-			for possibleMove in possibleMovesArray:
-				if self.graph[possibleMove].visited == False: 
-					self.graph[possibleMove].parentNode = self.graph[currentNode]
-					stack.append(possibleMove) 
-					self.graph[possibleMove].visited = True
-		#If Goal Found
-		if not(goal.id ==-1):
-			path = []
-			#Tranverse Parents of Goal Node till start Node is reached and print path
-			while not (goal is None):
-				#print(goal.id)
-				path.insert(0,goal)
-				goal = goal.parentNode
-			currentUbahnLine = path[0].Us[0]
-			drive1 = path[0].id
-			drive2 = path[0].id
-			costOfTrip = 0
-			for i in range(0,len(path)-1):
-				if(path[i+1].Us.__contains__(currentUbahnLine)):
-					costOfTrip+=path[i].possibleMovesCost[path[i].possibleMoves.index(path[i+1].id)]
-					drive2 = path[i+1].id
-					if i == len(path)-2:
-						if currentUbahnLine == 10:
-							currentUbahnLine= 55
-						print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
-				else:
-					costOfTrip +=5
-					print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
-					drive1 = drive2
-					drive2 = path[i+1].id
-					commonUbahnLine = set(path[i].Us).intersection(path[i+1].Us).pop()
-					print("Switch from U"+str(currentUbahnLine)+" To U"+str(commonUbahnLine))
-					currentUbahnLine = commonUbahnLine
-			print("Cost of Trip is :" + str(costOfTrip))
-		print("DFS searching time is " + str(time.time()-startTimeDFS))
-		print("Expanded Nodes : " + str(expandedNodes))
 	#Greedy Traversal
 	def Greedy(self,startNode,goalNode):
 		expandedNodes = 0
@@ -118,6 +59,9 @@ class Graph:
 		goal = Node(-1)
 		#while list not empty
 		while list:
+			for node in list:
+				print(allStations[node.id],end = "")
+			print("   /// List State ")
 			expandedNodes+=1
 			#remove element with lowest heuristic cost
 			currentNode = list[0]
@@ -177,7 +121,74 @@ class Graph:
 			print("Cost of Trip is :" + str(costOfTrip))	
 		print("Greedy Searching Time is " + str(time.time()-startTimeGreedy))
 		print("Expanded Nodes : " + str(expandedNodes))
-  #BFS Traversal 
+
+
+
+
+	#DFS Traversal 
+	def DFS(self,startNode,goalNode): 
+		expandedNodes = 0
+		startTimeDFS = time.time()
+		# Create a stack for DFS 
+		stack = [] 
+		#push first startNode to start traversing DFS
+		self.graph[startNode].visited = True
+		stack.append(startNode)
+		# Initialize goal Node
+		goal = Node(-1)
+        # traversing the stack until its empty
+		while stack: 
+			for node in stack:
+				print(allStations[node.id],end = "")
+			print("   /// Stack State ")
+			expandedNodes+=1
+			# pop an element
+			currentNode = stack.pop()
+			# check if it is a goal if it is break from the loop goal found
+			if int(goalNode) == int(currentNode):
+				goal = self.graph[currentNode]
+				break
+			# Get possible Moves for current Node
+			possibleMovesArray = self.graph[currentNode].possibleMoves
+			#push every possible move if not visited before
+			for possibleMove in possibleMovesArray:
+				if self.graph[possibleMove].visited == False: 
+					self.graph[possibleMove].parentNode = self.graph[currentNode]
+					stack.append(possibleMove) 
+					self.graph[possibleMove].visited = True
+		#If Goal Found
+		if not(goal.id ==-1):
+			path = []
+			#Tranverse Parents of Goal Node till start Node is reached and print path
+			while not (goal is None):
+				#print(goal.id)
+				path.insert(0,goal)
+				goal = goal.parentNode
+			currentUbahnLine = path[0].Us[0]
+			drive1 = path[0].id
+			drive2 = path[0].id
+			costOfTrip = 0
+			for i in range(0,len(path)-1):
+				if(path[i+1].Us.__contains__(currentUbahnLine)):
+					costOfTrip+=path[i].possibleMovesCost[path[i].possibleMoves.index(path[i+1].id)]
+					drive2 = path[i+1].id
+					if i == len(path)-2:
+						if currentUbahnLine == 10:
+							currentUbahnLine= 55
+						print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
+				else:
+					costOfTrip +=5
+					print("Ride U"+str(currentUbahnLine)+" from : "+str(allStations[drive1]+" to : "+ str(allStations[drive2])))
+					drive1 = drive2
+					drive2 = path[i+1].id
+					commonUbahnLine = set(path[i].Us).intersection(path[i+1].Us).pop()
+					print("Switch from U"+str(currentUbahnLine)+" To U"+str(commonUbahnLine))
+					currentUbahnLine = commonUbahnLine
+			print("Cost of Trip is :" + str(costOfTrip))
+		print("DFS searching time is " + str(time.time()-startTimeDFS))
+		print("Expanded Nodes : " + str(expandedNodes))
+
+	#BFS Traversal 
 	def BFS(self,startNode,goalNode): 
 		startTimeBFS = time.time()
 		expandedNodes= 0
@@ -190,6 +201,9 @@ class Graph:
 		goal = Node(-1)
         # traversing the queue until its empty
 		while queue: 
+			for node in queue:
+				print(allStations[node.id],end = "")
+			print("   /// Queue State ")
 			expandedNodes+=1
 			# Dequeue an element
 			currentNode = queue.pop(0)
@@ -274,4 +288,4 @@ for j in range(0,len(allStations)):
 	g.graph[j] = stationNode
 print("Creating Graph Time is " + str(time.time()-startTimeGraph))
 #DEBUGGING
-g.BFS(76,137)
+g.Greedy(0,100)
